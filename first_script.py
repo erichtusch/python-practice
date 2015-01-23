eeac5bb2a7a58d0a026a99fd2525778b731579dc
erichtusch <erich.tusch@gmail.com>
2015-01-23 14:00:41 -0500

    Add First Python script
    practicing printing and loops

---
+++ first_script.py
@@ -0,0 +1,12 @@
+
+printString = "hello world";
+loopEnd = 4;
+#   for loops syntax: "for i in range ([start], [end]):
+#   can also, rather than iterate, only select from a "collection" (array)
+for i in range (0,loopEnd):
+    # print can print multiple things, just seperate by commas
+    print printString, i+1;
+j=0;
+while j<=9:
+    print "printing a while loop %d times" % (j+1)
+    j +=1;
